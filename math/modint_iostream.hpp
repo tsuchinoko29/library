d107fd3d8529852c1a1ffa74a3aaf099df8563bc
nok0 <yanyanyanwan@gmail.com>
2022-05-01 12:04:18 +0900

    add mint

---
+++ math/modint_iostream.hpp
@@ -0,0 +1,22 @@
+#pragma once
+#include <iostream>
+
+#include "atcoder/modint"
+template<int m>
+std::istream &std::operator>>(std::istream &is, atcoder::static_modint<m> &a) {
+    long long v;
+    is >> v;
+    a = v;
+    return is;
+}
+template<int m>
+std::istream &std::operator>>(std::istream &is, atcoder::dynamic_modint<m> &a) {
+    long long v;
+    is >> v;
+    a = v;
+    return is;
+}
+template<int m>
+std::ostream &std::operator<<(std::ostream &os, const atcoder::static_modint<m> &a) { return os << a.val(); }
+template<int m>
+std::ostream &std::operator<<(std::ostream &os, const atcoder::dynamic_modint<m> &a) { return os << a.val(); }
